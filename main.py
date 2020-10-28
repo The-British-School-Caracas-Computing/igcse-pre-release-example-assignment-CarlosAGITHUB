b353b4cd72f2a45173c827b400787bff830cc20c
Carlos Aranguren <carlos.aranguren@tbscaracas.com>
2020-10-28 00:11:46 +0000

    Changed lists to nested dictionary

--- main.py
+++ main.py
@@ -1,10 +1,68 @@
-category = ["Phone", "Tablet", "SIM card", "Case", "Charger"]
-codes = ["BPCM", "BPSH", "RPSS", "RPLL", "YPLS", "YPLL", "RTMS","RTLM", "YTLM","YTLL","SMNO","SMPG","CSST","CSLX","CGCR","CGHM"]
-description = ["Compact","Clam Shell","RoboPhone – 5-inch screen and 64GB memory", "RoboPhone – 6-inch screen and 256GB memory", "Y-Phone Standard – 6-inch screen and 64GB memory", "Y-Phone Deluxe – 6-inch screen and 256GB memory", "RoboTab – 8-inch screen and 64GB memory", "RoboTab – 10-inch screen and 128GB memory", "Y-Tab Standard – 10-inch screen and 128GB memory", "Y-Tab Deluxe – 10-inch screen and 256GB memory", "SIM Free (no SIM card purchased)", "Pay As You Go (SIM card purchased)", "Standard", "Luxury", "Car", "Home"]
-price = ["29.99", "49.99", "199.99","499.99","549.99","649.99","149.99","299.99","499.99","599.99","0.00","50.00","19.99","15.99"]
+cc = {
+  "category" : {
+    "Phone": 1,
+    "Tablet": 2,
+    "SIM card": 3,
+    "Case": 4,
+    "Charger": 5
+  },
+  "codes" : {
+    "BPCM":1,
+    "BPSH":2,
+    "RPSS":3,
+    "RPLL":4,
+    "YPLS":5,
+    "YPLL":6,
+    "RTMS":7,
+    "RTLM":8,
+    "YTLM":9,
+    "YTLL":10,
+    "SMNO":11,
+    "SMPG":12,
+    "CSST":13,
+    "CSLX":14,
+    "CGCR":15,
+    "CGHM":16
+  },
+  "description" : {
+    "Compact": 1,
+    "Compact": 1,
+    "Clam Shell":1,
+    "RoboPhone – 5-inch screen and 64GB memory":1,
+    "RoboPhone – 6-inch screen and 256GB memory":1,
+    "Y-Phone Standard – 6-inch screen and 64GB memory":1,
+    "Y-Phone Deluxe – 6-inch screen and 256GB memory":1,
+    "RoboTab – 8-inch screen and 64GB memory":2,
+    "RoboTab – 10-inch screen and 128GB memory":2,
+    "Y-Tab Standard – 10-inch screen and 128GB memory":2,
+    "Y-Tab Deluxe – 10-inch screen and 256GB memory":2,
+    "SIM Free (no SIM card purchased)":3,
+    "Pay As You Go (SIM card purchased)":3,
+    "Standard":4,
+    "Luxury":4,
+    "Car":5,
+    "Home":5
+  },
+  "price" : {
+    "29.99":"a",
+    "49.99":"b",
+    "199.99":"c",
+    "499.99":"d",
+    "549.99":"e",
+    "649.99":"f",
+    "149.99":"g",
+    "299.99":"h",
+    "499.99":"i",
+    "599.99":"j",
+    "0.00": "k",
+    "50.00": "l",
+    "19.99": "m",
+    "15.99":"n"
+  },
+
 
 def categoryFunc():
-  for i in category:
+  for i in cc:
     print(i)
   userC = input("Which category are you interested in? ")
   if userC == "Phone":
